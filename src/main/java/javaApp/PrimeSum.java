bab9ed588077270b23955c4c52f4d5bb2747b8be
Autumn23 <36255295+Autumn23@users.noreply.github.com>
2020-05-17 00:57:11 +0100

    Update PrimeSum.java

--- src/main/java/javaApp/PrimeSum.java
+++ src/main/java/javaApp/PrimeSum.java
@@ -10,7 +10,25 @@ public class PrimeSum {
         int sum = 0, num;
         //insert your code here
 
-
+        while (num < maxNum)
+        {
+         i=2;
+         count=0;
+         while (i <= num/2)
+         {
+           if (num % i == 0)
+           {
+             count++;
+             break;
+           }
+           i++;
+         }
+         if (count == 0 && num!=1)
+         {
+           sum = sum+num;
+         }
+         num++;
+        }
         return sum;
     }
 } 
